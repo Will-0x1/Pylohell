cd16344c72224636055f2ec755ebd6ec20d239bc
Will-0x1 <33598026+Will-0x1@users.noreply.github.com>
2017-11-12 13:53:46 -0300

    Update function.py

--- function.py
+++ function.py
@@ -4,7 +4,6 @@ def Argumentos():
 	global args
 	parser = argparse.ArgumentParser()	
 	parser.add_argument("-pyl", dest="exploit", action="store", help="define o exploit que você irá usar!", required=False)
-	parser.add_argument("-host", dest="host", action="store", help="define o seu ip que irá usar!", required=False)
-	parser.add_argument("-lport", dest="port", action="store", help="define a porta que irá usar!", required=False)
-
+	parser.add_argument("-Host", dest="host", action="store", help="define o seu ip que irá usar!", required=False)
+	parser.add_argument("-Lport", dest="port", action="store", help="define a porta que irá usar!", required=False)
 	args = parser.parse_args()
