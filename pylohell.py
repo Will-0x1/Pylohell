531d51bf503a72cc189dd834e3482acc04471c04
Heaven <gilmarheavensilva@hotmail.com>
2017-11-12 10:16:14 -0300

    Update pylohell.py

--- pylohell.py
+++ pylohell.py
@@ -5,6 +5,13 @@ print("""
 \033[32m██╔═══╝   ╚██╔╝  ██║     ██║   ██║\033[0;0m\033[31m██╔══██║██╔══╝  ██║     ██║     \033[0;0m
 \033[32m██║        ██║   ███████╗╚██████╔╝\033[0;0m\033[31m██║  ██║███████╗███████╗███████╗\033[0;0m
 """)
+
+def Argumentos():
+	global args
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-e", dest="exploit", action="store", help="define o exploit que você irá usar!", required=False)
+	args = parser.parse_args()
+
 pr = str(input("Escolha Uma Opção: "))
 
 if pr == str('pyl'):
